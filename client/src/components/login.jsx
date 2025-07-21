e2328b8f8d189f96fc3deac834dda7d37f17b9ca
FullStackPranav <pranavpradeeptp@gmail.com>
2025-07-21 17:18:04 +0530

    prod error 1

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   const [showPassword, setShowPassword] = useState(false); 
 
   const API_BASE_URL = import.meta.env.VITE_API_URL;
-  // console.log(API_BASE_URL);
+console.log(API_BASE_URL);
 
   const handleChange = (e) => {
     setFormData(prev => ({
